a56d03cda1a78a8c2010d46849d0a224854fabe6
Anthony Cailleau <anthonycailleau.dev@gmail.com>
2025-04-23 21:35:50 +0200

    caroussel dynamique auto ok

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -20,29 +20,29 @@ const Bio = () => {
                             </div>
                         </div>
                     </div>
+                    <div className='bio-main'>
+                        <div className='bio-paragraph'>
+                            <p>
+                                Ben Bridgen is a composer, a multi instrumentalist (piano, guitar. bass, percussion and vocals), an arranger and a sound engineer. An Englishman abroad, whose native culture influences his spontaneous. self-taught and creative way of making music, setting him apart from his French counterparts. He has produced more than 25 albums of original music, of which he is often the composer, or co-composer and arranger. His curiosity for new discoveries, and his love for music in all its forms mean that he doesn't place restrictions on styles or influences, so he has worked in genres as diverse as French pop music with a string quartet, West Indian dancehall, not to mention pop rock, Latin American music, hip-hop, Malagasy music, punk, rock. even "musette", traditional French accordion based songs. All of the above with great sensitivity, great passion, and very high standards.
+                            </p>
+                            <p>
+                                He has also amassed considerable experience as a composer for multimedia, whether producing complete radio sound identities and jingles, music for television published by EMl, or collaborating on recording, advertising, and events. He has also done voice-over work for trailers and jingles, as well as occasionally singing for cartoons.
+                                Since 2018 he has been working with the parisian music publisher Cézame on creating new music for their catalogue.
+                            </p>
+                            <p>
+                                In 2022, Ben joined the contemporary dance company Le Collectif Mordu as resident composer and musician. Their first collaborative show
+                                "La fable de l'autruche" is touring now, and there is a new production planned in 2024 which will involve music being created and performed live on stage.
+                            </p>
+                        </div>
+                    </div>
                     <div className='bio-video-container'>
-                        <video controls width="100%" poster="/bio-cover.png">
+                        <video controls width="100%" poster='bio_play.png'>
                             <source src="https://www.dropbox.com/scl/fi/e18ww9z5mw14rgdrfu00v/Partition-d-un-Reve-VF.mov?rlkey=b987olus6c9shxmlt1lxoytu1&raw=1" type="video/mp4" />
                             Votre navigateur ne supporte pas la lecture de vidéo.
                         </video>
                     </div>
                 </div>
-                <div className='bio-main'>
 
-                    <div className='bio-paragraph'>
-                        <p>
-                            Ben Bridgen is a composer, a multi instrumentalist (piano, guitar. bass, percussion and vocals), an arranger and a sound engineer. An Englishman abroad, whose native culture influences his spontaneous. self-taught and creative way of making music, setting him apart from his French counterparts. He has produced more than 25 albums of original music, of which he is often the composer, or co-composer and arranger. His curiosity for new discoveries, and his love for music in all its forms mean that he doesn't place restrictions on styles or influences, so he has worked in genres as diverse as French pop music with a string quartet, West Indian dancehall, not to mention pop rock, Latin American music, hip-hop, Malagasy music, punk, rock. even "musette", traditional French accordion based songs. All of the above with great sensitivity, great passion, and very high standards.
-                        </p>
-                        <p>
-                            He has also amassed considerable experience as a composer for multimedia, whether producing complete radio sound identities and jingles, music for television published by EMl, or collaborating on recording, advertising, and events. He has also done voice-over work for trailers and jingles, as well as occasionally singing for cartoons.
-                            Since 2018 he has been working with the parisian music publisher Cézame on creating new music for their catalogue.
-                        </p>
-                        <p>
-                            In 2022, Ben joined the contemporary dance company Le Collectif Mordu as resident composer and musician. Their first collaborative show
-                            "La fable de l'autruche" is touring now, and there is a new production planned in 2024 which will involve music being created and performed live on stage.
-                        </p>
-                    </div>
-                </div>
             </div>
         </section>
     );
--- src/Studio.jsx
+++ src/Studio.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from 'react';
 import './Studio.scss';
-
+const images = ['caroussel_2.jpg', 'caroussel_3.jpg']
 const Studio = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [fade, setFade] = useState(true);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFade(false); // on commence par fondre l’image actuelle
+        }, 8000); // toutes les 4 secondes, pour bien laisser le temps de voir
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        if (!fade) {
+            const timeout = setTimeout(() => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+                setFade(true); // une fois l’image changée, on refait apparaître
+            }, 2000); // temps du fade-out (doit correspondre au CSS)
+
+            return () => clearTimeout(timeout);
+        }
+    }, [fade]);
     return (
+
         <div className='studio-container'>
             <div className='studio-header'>
                 <div className='studio-header-mobile-landscape'>
@@ -20,9 +42,27 @@ const Studio = () => {
                     </div>
                 </div>
                 <div className='studio-caroussel-container'>
+
                     <div className='studio-caroussel'>
-                        <img src="caroussel_1.jpg" alt="" />
+                        <svg width="60" height="60" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15 18L9 12L15 6" fill="white" />
+                        </svg>
+                        <img
+                            src={images[currentIndex]}
+                            alt={`carrousel ${currentIndex + 1}`}
+                            className={fade ? 'fade-in' : 'fade-out'}
+                        />
+                        <svg width="60" height="60" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9 6L15 12L9 18" fill="white" />
+                        </svg>
                     </div>
+
+                    <div className='studio-caroussel-description'>
+                        <p>Description 1</p>
+                        <p>Détails du matériel</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur incidunt eaque aliquid quaerat ipsum qui suscipit eveniet molestias reiciendis! Et suscipit dignissimos, iusto error laudantium placeat aliquam provident incidunt corporis.</p>
+                    </div>
+
                 </div>
             </div>
             <div className='studio-main'>
